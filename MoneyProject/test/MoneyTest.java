35375d150766b45d11f067cea383479ad20a8441
Nicholas Vaidyanathan <visionary.software.solutions@gmail.com>
2012-05-02 21:37:36 -0700

    starting on  * 2 feature

--- MoneyProject/test/MoneyTest.java
+++ MoneyProject/test/MoneyTest.java
@@ -6,10 +6,22 @@ public class MoneyTest {
     public void addFiveDollarsAndTenFrancs() {
         // Given : I have $5 and 10 CHF 
         
+        Franc 
         // And : The exchange rate is 2:1
         
         // When: I add the two together
         
         // Then: The result should be 10 dollars
+        
     }
+    
+    @Test
+    public void multiplyFiveDollarsTimesTwo(){
+        // Given : I have $5
+        Dollar five = new Dollar(5);
+        // When: I multiply it by 2
+        five.times(2);
+        // Then: I should have $10
+        assertEquals(10,five.amount);
+    } 
 }
---
+++ README.md
@@ -0,0 +1,21 @@
+To Do:
+- Make "amount" private
+- Dollar side-effects?
+- Money rounding?
+
+Features:
+- Multi-currency calculation
+Scenario: In order to be able to trade currency internationally 
+          As an investor
+		 I want to be able to add amounts of different currencies
+
+	Scenario: $5+ 10 CHF = $10 if exchange rate is 2:1
+	Given : I have $5 and 10 CHF
+	And : The exchange rate is 2:1
+	When : I add them together
+	Then : The result is $10
+	
+	Scenario: $5 *2
+	Given: I have $5
+	When : I multiply it by 2
+	Then: The result should be $10
