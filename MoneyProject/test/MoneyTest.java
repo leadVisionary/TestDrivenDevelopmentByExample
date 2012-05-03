3711f57418ab22e0bdda9aabe440c34baf433852
Nicholas Vaidyanathan <visionary.software.solutions@gmail.com>
2012-05-02 21:22:53 -0700

    starting currency conversion feature

---
+++ MoneyProject/test/MoneyTest.java
@@ -0,0 +1,15 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class MoneyTest {
+    @Test
+    public void addFiveDollarsAndTenFrancs() {
+        // Given : I have $5 and 10 CHF 
+        
+        // And : The exchange rate is 2:1
+        
+        // When: I add the two together
+        
+        // Then: The result should be 10 dollars
+    }
+}
